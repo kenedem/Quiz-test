b885afabad1b4a58d3b2ab1cafc091a8b2f6b848
kenedem <131464529+kenedem@users.noreply.github.com>
2023-04-21 14:09:17 +0000

    Add files via upload

---
+++ courage/js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does www stand for?",
+    answer: "world wide web",
+    options: [
+      "world wide web",
+      "web world wide",
+      "web wide world",
+      "wide world web"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which team won the champions league for 2018 seasion?",
+    answer: "Real Madrid FC",
+    options: [
+      "Barcelona FC",
+      "Bayern Munchen FC",
+      "Real Madrid FC",
+      "Paris Saint German FC"
+    ]
+  },
+    {
+      numb: 4,
+      question: "Which of the following is not a software threat?",
+      answer: "Hardware threat",
+      options: [
+        "Hardware threat",
+        "Electrical threat",
+        "Digital threat",
+        "Environmental threat"
+      ]
+  },
+    {
+    numb: 4,
+    question: "Which African country was colonized by the united kindom first ?",
+    answer: "Ghana",
+    options: [
+      "Liberia",
+      "Egypt",
+      "Ghana",
+      "South Africa"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Javascript is an _________ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The following are types of vulnerabilities except?",
+    answer: "Hardware vulnerabilities",
+    options: [
+      "Technological Vulnerabilities",
+      "Hardware vulnerabilities",
+      "Configuration Vulnerabilities",
+      "Security Policies Vulnerabilities"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is not a componebt of a server?",
+    answer: "Data",
+    options: [
+      "Security",
+      "Software",
+      "Data",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is not a Javascript framework?",
+    answer: "Cassandra",
+    options: [
+      "Node",
+      "Vue",
+      "React",
+      "Cassandra"
+    ]   
+  },
+  {
+    numb: 10,
+    question: "What is the synonym for fly?",
+    answer: "winged away",
+    options: [
+      "take off",
+      "winged away",
+      "disappear ",
+      "All of the above"
+    ]
+  }
+];
